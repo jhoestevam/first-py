cd4a613f36c1158e19d4eb870e1b9c8b3e3fc38c
Jhonatan Martins Estevam <jhonatanm222@gmail.com>
2017-06-07 22:28:23 -0300

    Arrumando problema com migrate
    Referenciando Blog em diretorio errado no arquivo settings.py
    
    Arrumando a atribuição as variaveis e parametros.

--- firstpython/firstpython/Blog/models.py
+++ firstpython/firstpython/Blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    author = models.ForeigKey('authorUser')
+    author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField();
     created_date = models.DateTimeField(default=timezone.now)
@@ -14,5 +14,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-        
+
 # Create your models here.
--- firstpython/firstpython/settings.py
+++ firstpython/firstpython/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.firstpython.Blog',
+    'firstpython.Blog',
 ]
 
 MIDDLEWARE = [
